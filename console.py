325851ee0768db1fb06523b9b2e1180a60f8bced
mahmoudnabil133 <hodanabil144@gmail.com>
2023-12-09 19:25:53 +0200

    console

--- console.py
+++ console.py
@@ -105,11 +105,7 @@ class HBNBCommand(cmd.Cmd):
         return cmd.Cmd.precmd(self, line)
 
     def do_create(self, line):
-        """
-        used to create new object
-        the command should be like that:
-        (create) + <class_Name>
-        """
+       
         if line:
             "parse line to separate class name"
             parsed = cmd.Cmd.parseline(self, line)
@@ -125,7 +121,11 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def do_show(self, line):
-        
+        """
+        used to show object by its id
+        command should be like that:
+        (show) + <class_Name> + <obj_id>
+        """
         if line:
             "parse line to separate class name and id in a touple"
             parsed = cmd.Cmd.parseline(self, line)

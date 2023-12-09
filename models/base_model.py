27bd14e8767a5496b416fd51053e5f8af51786c0
Mohanned-sameh <mohanned2678@gmail.com>
2023-12-09 15:57:23 +0200

    Revert "fixed base_model and fixed pycodestyle"
    This reverts commit 2283df15250e051e8c38bf5b4450b57fb66c3241.

--- models/base_model.py
+++ models/base_model.py
@@ -24,44 +24,37 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """
-        Initializes the BaseModel instance
+        Initialize the BaseModel class
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key in ("updated_at", "created_at"):
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                elif key[0] == "id":
-                    self.__dict__[key] = str(value)
-                else:
-                    self.__dict__[key] = value
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """
-        Prints the string representation of the BaseModel
+        Print the string representation of the BaseModel class
         """
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__,
-            self.id,
-            self.__dict__,
-        )
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
-        Update the public instance attribute updated_at
-        with the current datetime
+        Update the updated_at attribute with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all
-        keys/values of __dict__ of the instance
+        Return a dictionary containing all keys/values of
+        s__dict__ of the instance
         """
         map = {}
         for key, value in self.__dict__.items():
